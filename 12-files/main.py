075a2b25e3553408f1b6d2713c5c208b8a939092
John Hunt <john.hunt10@gmail.com>
2021-04-30 13:54:51 +0100

    Tidyied

--- 12-files/main.py
+++ 12-files/main.py
@@ -8,9 +8,15 @@ def main():
 	readings = load_data('data.csv')
 	for reading in readings:
 		print(reading)
-	dates = list(map(lambda r: r.date, readings))
+	# Obtain just the temperatures, dates and the indexes for each value
 	temperatures = list(map(lambda r: r.temp(), readings))
-	index = range(len(readings))
+	print('Temperatures:', temperatures)
+	dates = list(map(lambda r: r.date, readings))
+	print('Dates:', dates)
+
+	# Find all temperatures above 14.0
+	higher_temperatures = list(filter(lambda r: r.temp() > 14.0, readings))
+	print('Temperatures above 14.0:', higher_temperatures)
 
 	# Find minimum, maximum etc in readings
 	print('Min temp in list =', minimum(readings))
