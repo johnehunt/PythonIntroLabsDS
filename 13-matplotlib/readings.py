fb83d14d8aab0ec18526489e917d102b1d123e51
John Hunt <john.hunt10@gmail.com>
2021-04-30 11:09:08 +0100

    General tidying

--- 13-matplotlib/readings.py
+++ 13-matplotlib/readings.py
@@ -26,11 +26,11 @@ class Reading:
 
 	def __add__(self, other):
 		new_value = self.value + other.value
-		return RainfallReading(new_value, self.date, self.time, self.location)
+		return Reading(new_value, self.date, self.location)
 
 	def __sub__(self, other):
 		new_value = self.value - other.value
-		return RainfallReading(new_value, self.date, self.time, self.location)
+		return Reading(new_value, self.date, self.location)
 
 	def __str__(self):
 		return str(self.value) + ' on ' + str(self.date) + ' at ' + str(self.location)
@@ -46,6 +46,14 @@ class TemperatureReading(Reading):
 	def temp(self):
 		return self.value
 
+	def __add__(self, other):
+		new_value = self.value + other.value
+		return TemperatureReading(new_value, self.date, self.location, self.scale)
+
+	def __sub__(self, other):
+		new_value = self.value - other.value
+		return TemperatureReading(new_value, self.date, self.location, self.scale)
+
 	def __str__(self):
 		return 'TemperatureReading[' + self.scale + '](' + super().__str__() + ')'
 
@@ -54,8 +62,16 @@ class RainfallReading(Reading):
 	""" Subclass of Reading designed for timed rainfall readings """
 
 	def __init__(self, amount, date, time, location):
-		super().__init__(amount, date, time, location)
+		super().__init__(amount, date, location)
 		self.time = time
+
+	def __add__(self, other):
+		new_value = self.value + other.value
+		return RainfallReading(new_value, self.date, self.time, self.location)
+
+	def __sub__(self, other):
+		new_value = self.value - other.value
+		return RainfallReading(new_value, self.date, self.time, self.location)
 
 	def __str__(self):
 		return 'RainfallReading[' + self.time + '](' + super().__str__() + ')'
