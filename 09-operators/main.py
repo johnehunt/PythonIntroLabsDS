c1a1d5cd9fcbcc61f4e9ed5362e9e940590b4ff1
John Hunt <john.hunt10@gmail.com>
2021-05-20 16:18:56 +0100

    Added check for type being added

--- 09-operators/main.py
+++ 09-operators/main.py
@@ -1,5 +1,8 @@
 class TemperatureReading:
-	""" Specialised version of Reading to incorporates temperature info """
+	""" Class representing temperature info.
+	    It includes comparison operators
+	    which allow temperatures to be compared as well as added and subtracted.
+	"""
 
 	def __init__(self, temp, date, location, scale):
 		self.temp = temp
@@ -8,11 +11,17 @@ class TemperatureReading:
 		self.scale = scale
 
 	def __add__(self, other):
-		new_value = self.temp + other.temp
+		if isinstance(other, int) or isinstance(other, float):
+			new_value = self.temp + other
+		else:
+			new_value = self.temp + other.temp
 		return TemperatureReading(new_value, self.date, self.location, self.scale)
 
 	def __sub__(self, other):
-		new_value = self.temp - other.temp
+		if isinstance(other, int) or isinstance(other, float):
+			new_value = self.temp - other
+		else:
+			new_value = self.temp - other.temp
 		return TemperatureReading(new_value, self.date, self.location, self.scale)
 
 	def __eq__(self, other):
@@ -73,42 +82,50 @@ def celsius_to_fahrenheit(celsius):
 	return (celsius * 9 / 5) + 32
 
 
-def main():
-	# Set up the data the data file
-	readings = [
-			TemperatureReading(13.5, '01/05/20', 'London', 'Celsius'),
-			TemperatureReading(12.6, '02/05/20', 'London', 'Celsius'),
-			TemperatureReading(15.3, '03/05/20', 'London', 'Celsius'),
-			TemperatureReading(12.2, '04/05/20', 'London', 'Celsius'),
-			TemperatureReading(16.6, '05/05/20', 'London', 'Celsius'),
-			TemperatureReading(14.6, '05/05/20', 'London', 'Celsius'),
-			TemperatureReading(15.6, '05/05/20', 'London', 'Celsius')
-	]
+# Set up the data the data file
+readings = [
+		TemperatureReading(13.5, '01/05/20', 'London', 'Celsius'),
+		TemperatureReading(12.6, '02/05/20', 'London', 'Celsius'),
+		TemperatureReading(15.3, '03/05/20', 'London', 'Celsius'),
+		TemperatureReading(12.2, '04/05/20', 'London', 'Celsius'),
+		TemperatureReading(16.6, '05/05/20', 'London', 'Celsius'),
+		TemperatureReading(14.6, '05/05/20', 'London', 'Celsius'),
+		TemperatureReading(15.6, '05/05/20', 'London', 'Celsius')
+]
 
-	# Convert all the temperatures from Celsius to fahrenheit
-	fahrenheit_temperatures = list(map(lambda r: celsius_to_fahrenheit(r.temp), readings))
-	print('Fahrenheit Temperatures:', fahrenheit_temperatures)
+print('All Temperature Readings:')
+print(*readings, sep=", ")
 
-	# Obtain just the temperatures, dates and the indexes for each value
-	temperatures = list(map(lambda r: r.temp, readings))
-	print('Temperatures:', temperatures)
-	dates = list(map(lambda r: r.date, readings))
-	print('Dates:', dates)
+# Convert all the temperatures from Celsius to fahrenheit
+fahrenheit_temperatures = list(map(lambda r: celsius_to_fahrenheit(r.temp), readings))
+print('Fahrenheit Temperatures:', fahrenheit_temperatures)
 
-	# Find all temperatures above 14.0
-	higher_temperatures = list(filter(lambda r: r.temp > 14.0, readings))
-	print('Temperatures above 14.0:', higher_temperatures)
+# Obtain just the temperatures, dates and the indexes for each value
+temperatures = list(map(lambda r: r.temp, readings))
+print('Temperatures:', temperatures)
+dates = list(map(lambda r: r.date, readings))
+print('Dates:', dates)
 
-	# Find minimum, maximum etc in readings
-	print('Min temp in list =', minimum(readings))
-	print('Max temp in list =', maximum(readings))
-	print('Average temperature = {:.2f}'.format(average(readings)))
-	print('Median temperature value =', median(readings))
-	readings_range = data_range(readings)
-	print('Range of temperatures from ', str(readings_range[0].temp) + ' to ' + str(readings_range[1].temp))
+# Find all temperatures above 14.0
+higher_temperatures = list(filter(lambda r: r.temp > 14.0, readings))
+print('Temperatures above 14.0:', *higher_temperatures)
 
-	print('Done')
+# Find minimum, maximum etc in readings
+print('Min temp in list =', minimum(readings))
+print('Max temp in list =', maximum(readings))
+print('Average temperature = {:.2f}'.format(average(readings)))
+print('Median temperature value =', median(readings))
+readings_range = data_range(readings)
+print('Range of temperatures from ', str(readings_range[0].temp) + ' to ' + str(readings_range[1].temp))
 
+# Add temperatures together
+new_temperature = TemperatureReading(13.5, '01/05/20', 'London', 'Celsius') + TemperatureReading(15.5, '01/05/20', 'London', 'Celsius')
+print('Add two temperatures', new_temperature)
 
-if __name__ == '__main__':
-	main()
+new_temperature = TemperatureReading(13.5, '01/05/20', 'London', 'Celsius') + 5
+print('Add a temperature and a int', new_temperature)
+
+new_temperature = TemperatureReading(13.5, '01/05/20', 'London', 'Celsius') + 5.5
+print('Add a temperature and a float', new_temperature)
+
+print('Done')
