48e352e83953ce7335d19eacae8286f30779f4ed
John Hunt <john.hunt10@gmail.com>
2021-05-20 16:48:21 +0100

    Added rainfall examples

--- 10-inheritance/main.py
+++ 10-inheritance/main.py
@@ -48,14 +48,14 @@ class TemperatureReading(Reading):
 		if isinstance(other, int) or isinstance(other, float):
 			new_value = self.value + other
 		else:
-			new_value = self.value + other.temp
+			new_value = self.value + other.value
 		return TemperatureReading(new_value, self.date, self.location, self.scale)
 
 	def __sub__(self, other):
 		if isinstance(other, int) or isinstance(other, float):
 			new_value = self.value - other
 		else:
-			new_value = self.value - other.temp
+			new_value = self.value - other.value
 		return TemperatureReading(new_value, self.date, self.location, self.scale)
 
 	def __str__(self):
@@ -133,6 +133,9 @@ readings = [
 		TemperatureReading(15.6, '05/05/20', 'London', 'Celsius')
 ]
 
+print('All Temperature Readings:')
+print(*readings, sep=", ")
+
 # Convert all the temperatures from Celsius to fahrenheit
 fahrenheit_temperatures = list(map(lambda r: celsius_to_fahrenheit(r.value), readings))
 print('Fahrenheit Temperatures:', fahrenheit_temperatures)
@@ -154,5 +157,28 @@ print('Average temperature = {:.2f}'.format(average(readings)))
 print('Median temperature value =', median(readings))
 readings_range = data_range(readings)
 print('Range of temperatures from ', str(readings_range[0].value) + ' to ' + str(readings_range[1].value))
+
+# Add temperatures together
+new_temperature = TemperatureReading(13.5, '01/05/20', 'London', 'Celsius') + TemperatureReading(15.5, '01/05/20', 'London', 'Celsius')
+print('Add two temperatures', new_temperature)
+
+new_temperature = TemperatureReading(13.5, '01/05/20', 'London', 'Celsius') + 5
+print('Add a temperature and a int', new_temperature)
+
+new_temperature = TemperatureReading(13.5, '01/05/20', 'London', 'Celsius') + 5.5
+print('Add a temperature and a float', new_temperature)
+
+# Working with Rainfall readings
+rainfall_readings = [
+		RainfallReading(2.0, '01/05/20', '11:00', 'London'),
+		RainfallReading(2.6, '02/05/20', '11:30', 'London'),
+		RainfallReading(2.3, '03/05/20', '11:00', 'London'),
+		RainfallReading(3.2, '04/05/20', '12:00', 'London'),
+		RainfallReading(1.6, '05/05/20', '10:45', 'London')
+]
+
+print('All Fainfall Readings:')
+print(*rainfall_readings, sep=", ")
+print(f'Average rainfall {average(rainfall_readings)}')
 
 print('Done')
