f42f8409bf45a398acad53c65489a3fcec9c6c75
John Hunt <john.hunt10@gmail.com>
2021-05-20 16:58:47 +0100

    Updated file reading sample

--- 14-files/main.py
+++ 14-files/main.py
@@ -1,4 +1,8 @@
 from loader import load_data
+
+from readings import TemperatureReading
+from readings import RainfallReading
+from readings import InvalidTemperatureException
 from utils import *
 
 
@@ -6,8 +10,9 @@ def main():
 
 	# Load the data file
 	readings = load_data('data.csv')
-	for reading in readings:
-		print(reading)
+
+	print('All Temperature Readings:')
+	print(*readings, sep=", ")
 
 	# Convert all the temperatures from Celsius to fahrenheit
 	fahrenheit_temperatures = list(map(lambda r: celsius_to_fahrenheit(r.value), readings))
@@ -31,6 +36,35 @@ def main():
 	readings_range = data_range(readings)
 	print('Range of temperatures from ', str(readings_range[0].value) + ' to ' + str(readings_range[1].value))
 
+	# Add temperatures together
+	new_temperature = TemperatureReading(13.5, '01/05/20', 'London', 'Celsius') + TemperatureReading(15.5, '01/05/20',
+	                                                                                                 'London',
+	                                                                                                 'Celsius')
+	print('Add two temperatures', new_temperature)
+
+	new_temperature = TemperatureReading(13.5, '01/05/20', 'London', 'Celsius') + 5
+	print('Add a temperature and a int', new_temperature)
+
+	new_temperature = TemperatureReading(13.5, '01/05/20', 'London', 'Celsius') + 5.5
+	print('Add a temperature and a float', new_temperature)
+
+	# Working with Rainfall readings
+	rainfall_readings = [
+			RainfallReading(2.0, '01/05/20', '11:00', 'London'),
+			RainfallReading(2.6, '02/05/20', '11:30', 'London'),
+			RainfallReading(2.3, '03/05/20', '11:00', 'London'),
+			RainfallReading(3.2, '04/05/20', '12:00', 'London'),
+			RainfallReading(1.6, '05/05/20', '10:45', 'London')
+	]
+
+	print('All Rainfall Readings:')
+	print(*rainfall_readings, sep=", ")
+	print(f'Average rainfall {average(rainfall_readings)}')
+
+	try:
+		new_temperature = TemperatureReading(13.5, '01/05/20', 'London', 'Celsius') + '5.5'
+	except InvalidTemperatureException as e:
+		print(e)
 
 	print('Done')
 
