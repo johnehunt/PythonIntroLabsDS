d323f949eeab9ccfb63c57804ec81a1872847fa6
John Hunt <john.hunt10@gmail.com>
2021-05-20 15:28:52 +0100

    Extended the example

--- 05-collections/main.py
+++ 05-collections/main.py
@@ -11,9 +11,20 @@ while True:
 		readings.append(reading)
 
 print('Temperature readings input:', readings)
+print(f'There are {len(readings)} readings in total')
 
 readings.sort()
 print('Temperature readings sorted:', readings)
 
 readings.reverse()
 print('Temperature readings in reverse:', readings)
+
+print(f'There are {readings.count(0)} Zero readings')
+
+readings_copy = readings.copy()
+print('Copy of temperature readings:', readings_copy)
+readings_copy.sort()
+print('Temperature readings sorted copy:', readings_copy)
+print(f'Popping element from end of copy list {readings_copy.pop()}')
+print(f'Readings copy now contains {readings_copy}')
+print(f'Original list unchanged: {readings}')
