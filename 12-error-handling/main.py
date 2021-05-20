5f5e421e4be9fb9edb0773d6c401c37dce361273
John Hunt <john.hunt10@gmail.com>
2021-05-20 16:55:57 +0100

    Updated error handling example

--- 12-error-handling/main.py
+++ 12-error-handling/main.py
@@ -1,145 +1,7 @@
-class InvalidTemperatureException(Exception):
-	""" Valid Ages must be between 0 and 120 """
-
-	def __init__(self, value, message):
-		self.value = value
-		self.message = message
-
-	def __str__(self):
-		return f'InvalidTemperatureException({self.value}, {self.message})'
-
-
-class Reading:
-	"""Root class for all types of readings"""
-
-	def __init__(self, value, date, location):
-		self.value = value
-		self.date = date
-		self.location = location
-
-	def __eq__(self, other):
-		return self.value == other.value
-
-	def __ne__(self, other):
-		return self.value != other.value
-
-	def __ge__(self, other):
-		return self.value >= other.value
-
-	def __gt__(self, other):
-		return self.value > other.value
-
-	def __lt__(self, other):
-		return self.value < other.value
-
-	def __le__(self, other):
-		return self.value <= other.value
-
-	def __add__(self, other):
-		new_value = self.value + other.value
-		return Reading(new_value, self.date, self.location)
-
-	def __sub__(self, other):
-		new_value = self.value - other.value
-		return Reading(new_value, self.date, self.location)
-
-	def __str__(self):
-		return str(self.value) + ' on ' + str(self.date) + ' at ' + str(self.location)
-
-
-class TemperatureReading(Reading):
-	""" Specialised version of Reading to incorporates temperature info """
-
-	def __init__(self, temp, date, location, scale):
-		super().__init__(temp, date, location)
-		self.scale = scale
-
-	def __add__(self, other):
-		if isinstance(other, int) or isinstance(other, float):
-			new_value = self.value + other
-		elif isinstance(other, TemperatureReading):
-			new_value = self.value + other.value
-		else:
-			# Something is wrong its not an int, a float or a temperature reading
-			raise InvalidTemperatureException(other, 'Invalid type for addition to TemperatureReading')
-
-		return TemperatureReading(new_value, self.date, self.location, self.scale)
-
-	def __sub__(self, other):
-		if isinstance(other, int) or isinstance(other, float):
-			new_value = self.value - other
-		elif isinstance(other, TemperatureReading):
-			new_value = self.value - other.value
-		else:
-			raise InvalidTemperatureException(other, 'Invalid type for subtraction from TemperatureReading')
-		return TemperatureReading(new_value, self.date, self.location, self.scale)
-
-	def __str__(self):
-		return 'TemperatureReading[' + self.scale + '](' + super().__str__() + ')'
-
-
-class RainfallReading(Reading):
-	""" Subclass of Reading designed for timed rainfall readings """
-
-	def __init__(self, amount, date, time, location):
-		super().__init__(amount, date, location)
-		self.time = time
-
-	def __add__(self, other):
-		if isinstance(other, int) or isinstance(other, float):
-			new_value = self.value + other
-		elif isinstance(other, RainfallReading):
-			new_value = self.value + other.value
-		else:
-			raise InvalidTemperatureException(other, 'Invalid type for addition to RainfallReading')
-		return RainfallReading(new_value, self.date, self.time, self.location)
-
-	def __sub__(self, other):
-		if isinstance(other, int) or isinstance(other, float):
-			new_value = self.value - other
-		elif isinstance(other, RainfallReading):
-			new_value = self.value - other.value
-		else:
-			raise InvalidTemperatureException(other, 'Invalid type for subtraction from RainfallReading')
-		return RainfallReading(new_value, self.date, self.time, self.location)
-
-	def __str__(self):
-		return 'RainfallReading[' + self.time + '](' + super().__str__() + ')'
-
-
-def average(data):
-	if isinstance(data[0], int):
-		return sum(data) / len(data)
-	else:
-		raw_data = list(map(lambda r: r.value, data))
-		return sum(raw_data) / len(raw_data)
-
-
-def minimum(data):
-	return min(data)
-
-
-def maximum(data):
-	return max(data)
-
-
-def data_range(data):
-	return minimum(data), maximum(data)
-
-
-def median(data):
-	sorted_data = sorted(data)
-	data_length = len(data)
-	index = (data_length - 1) // 2
-
-	if data_length % 2:
-		return sorted_data[index]
-	else:
-		return (sorted_data[index] + sorted_data[index + 1]) / 2.0
-
-
-def celsius_to_fahrenheit(celsius):
-	return (celsius * 9 / 5) + 32
+from readings import TemperatureReading
+from readings import RainfallReading
+from readings import InvalidTemperatureException
+from utils import *
 
 
 def main():
@@ -209,8 +71,8 @@ def main():
 	except InvalidTemperatureException as e:
 		print(e)
 
+	print('Done')
 
-print('Done')
 
 if __name__ == '__main__':
 	main()
