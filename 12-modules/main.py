69668156635cff30e964c76b21be64bd2ced2ab4
John Hunt <john.hunt10@gmail.com>
2021-05-21 11:12:23 +0100

    Added module creation library

---
+++ 12-modules/main.py
@@ -0,0 +1,73 @@
+from readings import TemperatureReading
+from readings import RainfallReading
+from readings import InvalidTemperatureException
+from utils import *
+
+
+def main():
+	# Set up the data the data file
+	readings = [
+			TemperatureReading(13.5, '01/05/20', 'London', 'Celsius'),
+			TemperatureReading(12.6, '02/05/20', 'London', 'Celsius'),
+			TemperatureReading(15.3, '03/05/20', 'London', 'Celsius'),
+			TemperatureReading(12.2, '04/05/20', 'London', 'Celsius'),
+			TemperatureReading(16.6, '05/05/20', 'London', 'Celsius'),
+			TemperatureReading(14.6, '05/05/20', 'London', 'Celsius'),
+			TemperatureReading(15.6, '05/05/20', 'London', 'Celsius')
+	]
+
+	print('All Temperature Readings:')
+	print(*readings, sep=", ")
+
+	# Convert all the temperatures from Celsius to fahrenheit
+	fahrenheit_temperatures = list(map(lambda r: celsius_to_fahrenheit(r.value), readings))
+	print('Fahrenheit Temperatures:', fahrenheit_temperatures)
+
+	# Obtain just the temperatures, dates and the indexes for each value
+	temperatures = list(map(lambda r: r.value, readings))
+	print('Temperatures:', temperatures)
+	dates = list(map(lambda r: r.date, readings))
+	print('Dates:', dates)
+
+	# Find all temperatures above 14.0
+	higher_temperatures = list(filter(lambda r: r.value > 14.0, readings))
+	print('Temperatures above 14.0:', higher_temperatures)
+
+	# Find minimum, maximum etc in readings
+	print('Min temp in list =', minimum(readings))
+	print('Max temp in list =', maximum(readings))
+	print('Average temperature = {:.2f}'.format(average(readings)))
+	print('Median temperature value =', median(readings))
+	readings_range = data_range(readings)
+	print('Range of temperatures from ', str(readings_range[0].value) + ' to ' + str(readings_range[1].value))
+
+	# Add temperatures together
+	new_temperature = TemperatureReading(13.5, '01/05/20', 'London', 'Celsius') + TemperatureReading(15.5, '01/05/20',
+	                                                                                                 'London',
+	                                                                                                 'Celsius')
+	print('Add two temperatures', new_temperature)
+
+	new_temperature = TemperatureReading(13.5, '01/05/20', 'London', 'Celsius') + 5
+	print('Add a temperature and a int', new_temperature)
+
+	new_temperature = TemperatureReading(13.5, '01/05/20', 'London', 'Celsius') + 5.5
+	print('Add a temperature and a float', new_temperature)
+
+	# Working with Rainfall readings
+	rainfall_readings = [
+			RainfallReading(2.0, '01/05/20', '11:00', 'London'),
+			RainfallReading(2.6, '02/05/20', '11:30', 'London'),
+			RainfallReading(2.3, '03/05/20', '11:00', 'London'),
+			RainfallReading(3.2, '04/05/20', '12:00', 'London'),
+			RainfallReading(1.6, '05/05/20', '10:45', 'London')
+	]
+
+	print('All Rainfall Readings:')
+	print(*rainfall_readings, sep=", ")
+	print(f'Average rainfall {average(rainfall_readings)}')
+
+	print('Done')
+
+
+if __name__ == '__main__':
+	main()
---
+++ 12-modules/readings.py
@@ -0,0 +1,97 @@
+class InvalidTemperatureException(Exception):
+	""" Valid Ages must be between 0 and 120 """
+
+	def __init__(self, value, message):
+		self.value = value
+		self.message = message
+
+	def __str__(self):
+		return f'InvalidTemperatureException({self.value}, {self.message})'
+
+
+class Reading:
+	"""Root class for all types of readings"""
+
+	def __init__(self, value, date, location):
+		self.value = value
+		self.date = date
+		self.location = location
+
+	def __eq__(self, other):
+		return self.value == other.value
+
+	def __ne__(self, other):
+		return self.value != other.value
+
+	def __ge__(self, other):
+		return self.value >= other.value
+
+	def __gt__(self, other):
+		return self.value > other.value
+
+	def __lt__(self, other):
+		return self.value < other.value
+
+	def __le__(self, other):
+		return self.value <= other.value
+
+	def __add__(self, other):
+		new_value = self.value + other.value
+		return Reading(new_value, self.date, self.location)
+
+	def __sub__(self, other):
+		new_value = self.value - other.value
+		return Reading(new_value, self.date, self.location)
+
+	def __str__(self):
+		return str(self.value) + ' on ' + str(self.date) + ' at ' + str(self.location)
+
+
+class TemperatureReading(Reading):
+	""" Specialised version of Reading to incorporates temperature info """
+
+	def __init__(self, temp, date, location, scale):
+		super().__init__(temp, date, location)
+		self.scale = scale
+
+	def __add__(self, other):
+		if isinstance(other, int) or isinstance(other, float):
+			new_value = self.value + other
+		elif isinstance(other, TemperatureReading):
+			new_value = self.value + other.value
+		return TemperatureReading(new_value, self.date, self.location, self.scale)
+
+	def __sub__(self, other):
+		if isinstance(other, int) or isinstance(other, float):
+			new_value = self.value - other
+		elif isinstance(other, TemperatureReading):
+			new_value = self.value - other.value
+		return TemperatureReading(new_value, self.date, self.location, self.scale)
+
+	def __str__(self):
+		return 'TemperatureReading[' + self.scale + '](' + super().__str__() + ')'
+
+
+class RainfallReading(Reading):
+	""" Subclass of Reading designed for timed rainfall readings """
+
+	def __init__(self, amount, date, time, location):
+		super().__init__(amount, date, location)
+		self.time = time
+
+	def __add__(self, other):
+		if isinstance(other, int) or isinstance(other, float):
+			new_value = self.value + other
+		elif isinstance(other, RainfallReading):
+			new_value = self.value + other.value
+		return RainfallReading(new_value, self.date, self.time, self.location)
+
+	def __sub__(self, other):
+		if isinstance(other, int) or isinstance(other, float):
+			new_value = self.value - other
+		elif isinstance(other, RainfallReading):
+			new_value = self.value - other.value
+		return RainfallReading(new_value, self.date, self.time, self.location)
+
+	def __str__(self):
+		return 'RainfallReading[' + self.time + '](' + super().__str__() + ')'
