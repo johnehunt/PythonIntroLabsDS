8cea54f4d33938463bc830eb459de6f371dcdff1
John Hunt <john.hunt10@gmail.com>
2021-05-21 14:18:06 +0100

    Min changes

--- 05-collections/main.py
+++ 05-collections/main.py
@@ -4,27 +4,31 @@ while True:
 	input_string = input('Please enter a temperature reading (-1 to end): ')
 	if input_string == '-1':
 		break
-	elif not input_string.replace(".", "").isdigit():
-		print('Must be a positive number')
+	elif input_string.count('.') > 1 or not input_string.replace(".", "").isdigit():
+		print('Must be a positive floating point number')
 	else:
 		reading = float(input_string)
 		readings.append(reading)
 
 print('Temperature readings input:', readings)
+
 print(f'There are {len(readings)} readings in total')
 
 readings.sort()
-print('Temperature readings sorted:', readings)
+print(f'Temperature readings sorted: {readings}')
 
 readings.reverse()
 print('Temperature readings in reverse:', readings)
 
-print(f'There are {readings.count(0)} Zero readings')
+print(f'There are {readings.count(0.0)} Zero readings')
 
 readings_copy = readings.copy()
 print('Copy of temperature readings:', readings_copy)
-readings_copy.sort()
-print('Temperature readings sorted copy:', readings_copy)
+
+readings_copy.append(5.5)
+
+print('Temperature readings:', readings)
+print('Copy of Temperature readings:', readings_copy)
+
 print(f'Popping element from end of copy list {readings_copy.pop()}')
 print(f'Readings copy now contains {readings_copy}')
-print(f'Original list unchanged: {readings}')
