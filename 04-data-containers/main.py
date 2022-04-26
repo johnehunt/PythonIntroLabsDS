f3a3d31175cdd4f5eab1949b3fd1aefb9afa63dd
John Hunt <john.hunt10@gmail.com>
2022-04-26 16:14:08 +0100

    reformatted

--- 04-data-containers/main.py
+++ 04-data-containers/main.py
@@ -1,14 +1,14 @@
 readings = []
 
 while True:
-	input_string = input('Please enter a temperature reading (-1 to end): ')
-	if input_string == '-1':
-		break
-	elif input_string.count('.') > 1 or not input_string.replace('.', '').isnumeric():
-		print('Must be a positive floating point number')
-	else:
-		reading = float(input_string)
-		readings.append(reading)
+    input_string = input('Please enter a temperature reading (-1 to end): ')
+    if input_string == '-1':
+        break
+    elif input_string.count('.') > 1 or not input_string.replace('.', '').isnumeric():
+        print('Must be a positive floating point number')
+    else:
+        reading = float(input_string)
+        readings.append(reading)
 
 print('Temperature readings input:', readings)
 
