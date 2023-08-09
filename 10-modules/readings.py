fccad72169f317fd7a63173f9edcd7a39f858ddc
John Hunt <john.hunt10@gmail.com>
2023-08-09 10:54:07 +0100

    Updated with repr

--- 10-modules/readings.py
+++ 10-modules/readings.py
@@ -5,114 +5,111 @@ CELSIUS = "Celsius"
 FAHRENHEIT = "Fahrenheit"
 
 class Reading:
-	"""Root class for all types of readings"""
+    """Root class for all types of readings"""
 
-	def __init__(self, value, date, location):
-		self.value = value
-		self.date = date
-		self.location = location
+    def __init__(self, value, date, location):
+        self.value = value
+        self.date = date
+        self.location = location
 
-	def __eq__(self, other):
-		return self.value == other.value
+    def __eq__(self, other):
+        return self.value == other.value
 
-	def __ne__(self, other):
-		return self.value != other.value
+    def __ne__(self, other):
+        return self.value != other.value
 
-	def __ge__(self, other):
-		return self.value >= other.value
+    def __ge__(self, other):
+        return self.value >= other.value
 
-	def __gt__(self, other):
-		return self.value > other.value
+    def __gt__(self, other):
+        return self.value > other.value
 
-	def __lt__(self, other):
-		return self.value < other.value
+    def __lt__(self, other):
+        return self.value < other.value
 
-	def __le__(self, other):
-		return self.value <= other.value
+    def __le__(self, other):
+        return self.value <= other.value
 
-	def __add__(self, other):
-		new_value = self.value + other.value
-		return Reading(new_value, self.date, self.location)
+    def __add__(self, other):
+        new_value = self.value + other.value
+        return Reading(new_value, self.date, self.location)
 
-	def __sub__(self, other):
-		new_value = self.value - other.value
-		return Reading(new_value, self.date, self.location)
+    def __sub__(self, other):
+        new_value = self.value - other.value
+        return Reading(new_value, self.date, self.location)
 
-	def __str__(self):
-		return str(self.value) + ' on ' + str(self.date) + ' at ' + str(self.location)
+    def __repr__(self):
+        return f'value={self.value}, date={self.date}, location={self.location}'
+
+    def __str__(self):
+        return str(self.value) + ' on ' + str(self.date) + ' at ' + str(self.location)
 
 
 class TemperatureReading(Reading):
-	""" Specialised version of Reading to incorporates temperature info """
+    """ Specialised version of Reading to incorporates temperature info """
 
-	def __init__(self, temp, date, location, scale):
-		super().__init__(temp, date, location)
-		self.scale = scale
+    def __init__(self, temp, date, location, scale):
+        super().__init__(temp, date, location)
+        self.scale = scale
 
-	def convert(self):
-		""" convert the temperature to a different scale """
-		print(self.scale)
-		print(CELSIUS)
-		print(self.scale == CELSIUS)
-		if self.scale == CELSIUS:
-			return TemperatureReading(celsius_to_fahrenheit(self.value),
-			                          self.date,
-			                          self.location,
-			                          FAHRENHEIT)
-		else:
-			return TemperatureReading(fahrenheit_to_celsius(self.value),
-			                          self.date,
-			                          self.location,
-			                          CELSIUS)
+    def convert(self):
+        """ convert the temperature to a different scale """
+        if self.scale == CELSIUS:
+            return TemperatureReading(celsius_to_fahrenheit(self.value),
+                                      self.date,
+                                      self.location,
+                                      FAHRENHEIT)
+        else:
+            return TemperatureReading(fahrenheit_to_celsius(self.value),
+                                      self.date,
+                                      self.location,
+                                      CELSIUS)
 
-	def __add__(self, other):
-		new_value = None
-		if isinstance(other, int) or isinstance(other, float):
-			new_value = self.value + other
-		elif isinstance(other, TemperatureReading):
-			new_value = self.value + other.value
-		return TemperatureReading(new_value, self.date, self.location, self.scale)
+    def __add__(self, other):
+        if isinstance(other, int) or isinstance(other, float):
+            new_value = self.value + other
+        else:
+            new_value = self.value + other.value
+        return TemperatureReading(new_value, self.date, self.location, self.scale)
 
-	def __sub__(self, other):
-		new_value = None
-		if isinstance(other, int) or isinstance(other, float):
-			new_value = self.value - other
-		elif isinstance(other, TemperatureReading):
-			new_value = self.value - other.value
-		return TemperatureReading(new_value, self.date, self.location, self.scale)
+    def __sub__(self, other):
+        if isinstance(other, int) or isinstance(other, float):
+            new_value = self.value - other
+        else:
+            new_value = self.value - other.value
+        return TemperatureReading(new_value, self.date, self.location, self.scale)
 
-	def __repr__(self):
-		return f'TemperatureReading({self.value}, {self.date}, {self.location}, {self.scale})'
+    def __repr__(self):
+        return f'TemperatureReading({super().__repr__()}, {self.scale})'
 
-	def __str__(self):
-		return 'TemperatureReading[' + self.scale + '](' + super().__str__() + ')'
+    def __str__(self):
+        return 'TemperatureReading[' + self.scale + '](' + super().__str__() + ')'
 
 
 class RainfallReading(Reading):
-	""" Subclass of Reading designed for timed rainfall readings """
+    """ Subclass of Reading designed for timed rainfall readings """
 
-	def __init__(self, amount, date, time, location):
-		super().__init__(amount, date, location)
-		self.time = time
+    def __init__(self, amount, date, time, location):
+        super().__init__(amount, date, location)
+        self.time = time
 
-	def __add__(self, other):
-		new_value = None
-		if isinstance(other, int) or isinstance(other, float):
-			new_value = self.value + other
-		elif isinstance(other, RainfallReading):
-			new_value = self.value + other.value
-		return RainfallReading(new_value, self.date, self.time, self.location)
+    def __add__(self, other):
+        if isinstance(other, int) or isinstance(other, float):
+            new_value = self.value + other
+        else:
+            new_value = self.value + other.temp
+        return RainfallReading(new_value, self.date, self.time, self.location)
 
-	def __sub__(self, other):
-		new_value = None
-		if isinstance(other, int) or isinstance(other, float):
-			new_value = self.value - other
-		elif isinstance(other, RainfallReading):
-			new_value = self.value - other.value
-		return RainfallReading(new_value, self.date, self.time, self.location)
+    def __sub__(self, other):
+        if isinstance(other, int) or isinstance(other, float):
+            new_value = self.value - other
+        else:
+            new_value = self.value - other.temp
+        return RainfallReading(new_value, self.date, self.time, self.location)
 
-	def __repr__(self):
-		return f'RainfallReading({self.value}, {self.date}, {self.time}, {self.location})'
+    def __repr__(self):
+        return f'RainfallReading {super().__repr__()}, time={self.time})'
 
-	def __str__(self):
-		return 'RainfallReading[' + self.time + '](' + super().__str__() + ')'
+    def __str__(self):
+        return 'RainfallReading[' + self.time + '](' + super().__str__() + ')'
+
