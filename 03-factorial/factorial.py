2d6a8c71de47b00909d98ef77a1d456d1d8bca0f
John Hunt <john.hunt10@gmail.com>
2023-09-19 16:20:27 +0100

    Simplified solution

--- 03-factorial/factorial.py
+++ 03-factorial/factorial.py
@@ -2,15 +2,11 @@ print('Starting factorial calculation program')
 input_number = input('Please input the number: ')
 print(f'The number to calculate factorial for is {input_number}')
 
-if input_number.startswith('-'):
-    print('Factorial not defined for negative numbers')
-elif input_number.isnumeric():
+if input_number.isnumeric():
     num = int(input_number)
 
     if num == 0:
         print('0! factorial is 1')
-    elif num == 1:
-        print('1! factorial is 1')
     else:
         # Loop element
         factorial = 1
